b99c4e5bab439dd5760d4109009dcdd6263d5935
JitBanerjee03 <Jitbanerjee294@gmail.com>
2024-07-27 20:48:54 +0530

    Adding the files

---
+++ L7/index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const fs = require('node:fs');
+
+app.set('view engine',"ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get("/",(req,res)=>{
+  fs.readdir('./files',(err,files)=>{
+    res.render("index",{files:files});
+  });
+})
+
+app.post("/create",(req,res)=>{
+  //const name=req.body.title.split(' ').join('');
+  fs.writeFile("./files/File1.txt",req.body.details,'utf-8',(err)=>{
+     res.redirect('/');
+  });
+})
+app.listen(3000);
