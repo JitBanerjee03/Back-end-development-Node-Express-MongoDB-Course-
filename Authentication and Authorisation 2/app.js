b897b61ec6fa02dc1bf5709ca3cacf776fafdff7
JitBanerjee03 <Jitbanerjee294@gmail.com>
2024-07-29 00:22:31 +0530

    Adding the first lecture 2 of authentication and authorisation

---
+++ Authentication and Authorisation 2/app.js
@@ -0,0 +1,61 @@
+const express=require('express');
+const app=express();
+
+const userModel=require('./models/user');
+
+const path=require('path');
+
+const bcrypt=require('bcrypt');
+
+const cookieParser=require('cookie-parser');
+
+const jwt=require('jsonwebtoken');
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+   res.render('index');
+});
+
+app.post('/create',(req,res)=>{
+    
+     
+    let{username,email,password,age}=req.body;
+    
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt,async(err,hash)=>{
+           console.log(hash);
+
+           let createdUser=await userModel.create({
+            username,
+            email,
+            password:hash,
+            age
+          });
+           let token=jwt.sign({email},"shhhhhhhhhhhhhhh");
+           res.cookie("token",token);
+           res.send(createdUser);
+        });
+    })
+ });
+
+ app.get('/login',(req,res)=>{
+    res.render('login');
+})
+
+app.post('/login',async (req,res)=>{
+    let user=await userModel.findOne({email:req.body.email});
+
+    if(!user) return res.send("Something went wrong");
+})
+
+ app.get('/logout',(req,res)=>{
+  res.cookie("token","");
+  res.redirect('/');
+})
+
+app.listen(3000);
